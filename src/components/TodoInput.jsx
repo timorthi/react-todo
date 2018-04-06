41d3de6a3496f5cb2e65eb37c27eda7341c5b167
Timothy Ng <5664347+timorthi@users.noreply.github.com>
2018-04-05 23:49:11 -0700

    Add ability to add item to an internal todo list (#3)
    * Add to internal items list when add button clicked
    
    * Rewrite component functions as arrow functions

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import { Container, Button, Input } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 
 export default class TodoInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: "" };
+    }
+
+    setParentState = () => {
+        this.props.onSubmit(this.state.value);
+        this.setState({ value: "" });
+    };
+
+    handleInputChange = (e) => {
+        this.setState({ value: e.target.value });
+    };
+
     render() {
         return (
             <Container>
-                <Input placeholder="Enter something to do..." />
-                <Button>
+                <Input
+                    value={this.state.value}
+                    onChange={this.handleInputChange}
+                    placeholder="Enter something to do..."
+                />
+                <Button onClick={this.setParentState}>
                     Add
                 </Button>
             </Container>
         );
     }
+}
+
+TodoInput.propTypes = {
+    onSubmit: PropTypes.func.isRequired
 }
--- src/containers/TodoList.jsx
+++ src/containers/TodoList.jsx
@@ -4,10 +4,21 @@ import TodoInput from '../components/TodoInput';
 import TodoItems from '../components/TodoItems';
 
 export default class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { items: [] };
+    }
+
+    addItem = (item) => {
+        var items = this.state.items;
+        items.push(item);
+        this.setState({ items: items });
+    };
+
     render() {
         return (
             <Container as="div">
-                <TodoInput />
+                <TodoInput onSubmit={this.addItem} />
                 <TodoItems />
             </Container>
         );
