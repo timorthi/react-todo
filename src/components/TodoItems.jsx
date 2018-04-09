73b3952141013ac990615afcd1a33ddfd0bffa2b
Timothy Ng <timnjj@gmail.com>
2018-04-09 14:22:12 -0700

    Create TodoItem component

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
 import { Container, List } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
+import TodoItem from '../components/TodoItem';
 
 export default class TodoItems extends Component {
     render() {
         return (
             <Container>
                 <List>
-                    { this.props.items.map((item) => {
-                        return (
-                            <List.Item key={item}>
-                                {item}
-                            </List.Item>
-                        );
-                    }) }
+                    { this.props.items.map((item) => { return <TodoItem item={item} />; }) }
                 </List>
             </Container>
         );
