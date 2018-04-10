c82774b72a2705fd2ea87cbd4961f8081d230c82
Timothy Ng <timnjj@gmail.com>
2018-04-10 12:07:45 -0700

    Add basic redux structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
-import App from './views/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import App from './views/App';
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
--- src/views/App.js
+++ src/views/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import './App.css';
 import TodoList from './TodoList';
+
+import reducers from '../redux';
+
+const store = createStore(reducers);
 
 export default class App extends Component {
   render() {
     return (
-      <TodoList />
+      <Provider store={store}>
+        <TodoList />
+      </Provider>
     );
   }
 }
