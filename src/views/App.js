026980201fd1d25861ea850b67ee6cd493688775
Timothy Ng <timnjj@gmail.com>
2018-04-12 11:07:23 -0700

    Add redux-logger

--- src/views/App.js
+++ src/views/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import './App.css';
 import TodoList from './TodoList';
 
 import reducers from '../redux';
 
-const store = createStore(reducers);
+let middlewares = [];
+if (process.env.NODE_ENV === 'development') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
+const store = createStore(reducers, applyMiddleware(...middlewares));
 
 export default class App extends Component {
   render() {
